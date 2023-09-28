440d943425c09176a2e628715078220d857fd5cc
Pikin <pikin@bk.ru>
2023-09-28 12:19:00 +0500

    getInfo changed

--- src/Main/App.java
+++ src/Main/App.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         String str = getInfo();
-        checkStringLength(str);
+
     }
 
     public static String getInfo() {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         scanner.close();
+        checkStringLength(str);
         return str;
     }
 
-    public static boolean checkStringLength(String str) {
+    public static void checkStringLength(String str) {
         String[] strArray = str.split(" ");
         if (strArray.length < 6) {
             throw new RuntimeException("Количество введенных данных меньше, чем необходимо");
         } else if (strArray.length > 6) {
             throw new RuntimeException("Количество введенных данных больше, чем необходимо");
         }
-        return true;
+
     }
 }
