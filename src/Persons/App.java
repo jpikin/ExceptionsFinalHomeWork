5653628396d911945977fc20d7f1da16715b3db7
pikin <pikin@bk.ru>
2023-10-03 00:09:36 +0500

    done

--- src/Persons/App.java
+++ src/Persons/App.java
@@ -11,7 +11,7 @@ public class App {
 
         Person newPerson = new Person(getPersonInfo());
         FilePersons filePersons = new FilePersons();
-        filePersons.checkListPersons(newPerson);
+        filePersons.createNewFilePerson(newPerson);
     }
     public static List<List> getPersonInfo() {
         Scanner scanner = new Scanner(System.in);
--- src/Persons/FilePersons.java
+++ src/Persons/FilePersons.java
@@ -5,49 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FilePersons {
-    private List listPersons = new ArrayList();
+
 
     public FilePersons() throws IOException {
-        listPersons = readListPersons();
+
     }
 
-    public List getListPersons() {
-        return listPersons;
-    }
-
-    public List readListPersons() throws IOException {
-        try (FileReader reader = new FileReader("listPersons.txt")) {
-            int c;
-            while ((c = reader.read()) != -1) {
-                listPersons.add((char) c);
-            }
-        } catch (FileNotFoundException ex) {
-            throw new FileNotFoundException("Файл не найден");
-        }
-        return listPersons;
-    }
-
-    public void checkListPersons(Person person) {
-        String name = person.getLastName();
-        boolean flag = false;
-        if (listPersons != null) {
-            for (Object s : listPersons) {
-                if (s.equals(name)) flag = true;
-            }
-            if (flag) addNewPersonToFile(person);
-            else createNewFilePerson(person);
-
-        } else {
-            createNewFilePerson(person);
-            try (FileWriter writer = new FileWriter("listPersons.txt", true)) {
-                String text = person.getLastName()+"\n";
-                writer.write(text);
-                writer.flush();
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
 
     public void createNewFilePerson(Person person) {
 
@@ -60,13 +23,5 @@ public class FilePersons {
         }
     }
 
-    public void addNewPersonToFile(Person person) {
-        try (FileWriter writer = new FileWriter(person.getLastName() + ".txt", false)) {
-            String text = person.getAllPersonInfo()+"\n";
-            writer.write(text);
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+
 }
