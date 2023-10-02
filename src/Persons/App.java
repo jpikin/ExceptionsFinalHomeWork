5e9c18c07800fe3f46b41222b1dadb82a9d49726
pikin <pikin@bk.ru>
2023-10-03 00:12:36 +0500

    done

--- src/Persons/App.java
+++ src/Persons/App.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Person newPerson = new Person(getPersonInfo());
         FilePersons filePersons = new FilePersons();
